1aeb0619f76dc4b8c255ddf5a920e550bdea64c5
LucioRode <luciorode@gmail.com>
2023-07-01 16:21:06 -0300

    Iniciando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
+import { ContactoComponent } from './Componentes/contacto/contacto.component';
+import { ResultadosComponent } from './Componentes/resultados/resultados.component';
+import { ComisionComponent } from './Componentes/comision/comision.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { BannerComponent } from './Componentes/banner/banner.component';
+import { BtnsComponent } from './Componentes/btns/btns.component';
+import { BlogComponent } from './Componentes/blog/blog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContactoComponent,
+    ResultadosComponent,
+    BlogComponent,
+    ComisionComponent,
+    InicioComponent,
+    BannerComponent,
+    BtnsComponent,
   ],
   imports: [
     BrowserModule,
